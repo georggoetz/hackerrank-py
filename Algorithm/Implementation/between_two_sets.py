6cbc0201e09534ee821ffdd2241942bcd8aaa196
georgg <GeorgG@haufe.com>
2017-12-13 00:38:40 +0100

    Cleaning up Between Two Sets

--- Algorithm/Implementation/between_two_sets.py
+++ Algorithm/Implementation/between_two_sets.py
@@ -15,9 +15,8 @@ def getTotalX(a, b):
 
 
 if __name__ == "__main__":
-    n, m = input().strip().split(' ')
-    n, m = [int(n), int(m)]
-    a = list(map(int, input().strip().split(' ')))
-    b = list(map(int, input().strip().split(' ')))
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
     total = getTotalX(a, b)
     print(total)
